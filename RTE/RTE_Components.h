349b050b663421ddeaf76f1e5ac4e052021e52e5
JureValjavec <valjavecjur@gmail.com>
2016-11-03 14:05:24 +0100

    Initial version

---
+++ RTE/RTE_Components.h
@@ -0,0 +1,14 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'Basic_Atmel_SAMD_Project' 
+ * Target:  'SAMD20J18' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+#endif /* RTE_COMPONENTS_H */
---
+++ main.c
@@ -0,0 +1,12 @@
+#include "SAMD20.h"
+#include "main.h"
+
+
+int main (void)
+{
+	
+	while (1)
+	{
+	
+	}
+}
